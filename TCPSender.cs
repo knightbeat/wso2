fe300bd8f0ca0c5cb96e36f96057de0c7191649c
Chathura Kulasinghe <divinecorps@gmail.com>
2015-07-21 13:56:17 -0300

    TCP Sender code in C#
    Can be used to publish TCP messages into WSO2 ESB

---
+++ TCPSender.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Communication.Tcp
+{
+    class TCPSender
+    {
+        static void Main(string[] args)
+        {
+            string input = "Sample text comes form dot net TEXT VALUE";
+            string IpAddressOfESB = "192.168.56.101";
+            int TCPPortOfESB = 6061;
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(IpAddressOfESB), TCPPortOfESB);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ip);
+                Console.WriteLine("Connected server.");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Unable to connect to server.");
+                return;
+            }
+            server.Send(Encoding.ASCII.GetBytes(input));
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
+    }
+}
